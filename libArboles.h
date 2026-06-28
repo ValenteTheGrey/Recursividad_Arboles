16d1f73824d3c181743cea6647aeaa5098f71daf
ValenteTheGrey <155317236+ValenteTheGrey@users.noreply.github.com>
2026-06-28 12:04:30 -0300

    Update libArboles.h

--- libArboles.h
+++ libArboles.h
@@ -7,14 +7,14 @@
 
 
 //Arboles
-typedef struct s_nodoa
+typedef struct sNodoa
 {
     void* info;
     size_t tam_info;
-    struct s_nodoa *izq, *der;
-}t_nodoa;
+    struct sNodoa *izq, *der;
+}tNodoa;
 
-typedef t_nodoa* t_arbol;
+typedef tNodoa* tArbol;
 
 
 double factorial(unsigned n);
@@ -22,9 +22,9 @@ int mcd (int m, int n);
 void imp_cad1(char* cad);
 int cmpInt(const void* a, const void* b);
 void impInt(void* numImp, void* argInutil);
-void iniciar_arbol(t_arbol* pa);
-int insertar_arbol(t_arbol* pa, void* pd, size_t tam, int (*cmp)(const void*, const void*));
-void rec_en_orden(t_arbol *pa, void (*accion)(void *, void*), void * params);
+void iniciar_arbol(tArbol* pa);
+int insertar_arbol(tArbol* pa, void* pd, size_t tam, int (*cmp)(const void*, const void*));
+void rec_en_orden(tArbol *pa, void (*accion)(void*, void*), void* params);
 
 
 #endif // LIBARBOLES_H_INCLUDED
